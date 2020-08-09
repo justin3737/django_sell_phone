740f7c34519098da94cef2dc7c4f2e9782106152
Wu Justin <justin.cathay@gmail.com>
2020-08-09 18:25:49 +0800

    加入disp detail

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from mysite.models import Product
 
 # Create your views here.
+def disp_detail(request, sku):
+    html = '''
+<html>
+<head>
+<title>{}</title>
+</head>
+<body>
+<h2>{}</h2>
+<hr>
+<table width=400 border=1 bgcolor='#ccffcc'>
+{}
+</table>
+<a href='/list'>回列表</a>
+</body>
+</html>
+'''
+    try:
+        p = Product.objects.get(sku=sku)
+    except Product.DoesNotExist:
+        raise Http404('找不到指定的品項編號')
+    tags = '<tr><td>品項編號</td><td>{}</td></tr>'.format(p.sku)
+    tags = tags + '<tr><td>品項名稱</td><td>{}</td></tr>'.format(p.name)
+    tags = tags + '<tr><td>價格</td><td>{}</td></tr>'.format(p.price)
+    tags = tags + '<tr><td>庫存數量</td><td>{}</td></tr>'.format(p.qty)
+    return HttpResponse(html.format(p.name, p.name, tags))
+
+
 def about(request):
     html = '''
 <html>
