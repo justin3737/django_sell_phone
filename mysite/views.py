44a4791576fbf48a63c0c61d9e366c30817238f4
Wu Justin <justin.cathay@gmail.com>
2020-08-09 17:56:43 +0800

    add about/

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def about(request):
+    html = '''
+<html>
+<head>
+<title>About Myself</title>
+</head>
+<body>
+<h2>Justin Wu</h2>
+<hr>
+<p>
+Hi! I'm Justin Wu,  Nice to meet you.
+</p>
+</body>
+</html>
+'''
+
+    return HttpResponse(html)
